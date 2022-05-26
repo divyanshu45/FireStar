2b998197a93b5998c74d7a878ed94f5f75e07a10
divyanshu45 <mdivyanshu996@gmail.com>
2022-05-26 23:57:38 +0530

    API added

--- app/src/main/java/com/example/firestar/auth/LoginActivity.kt
+++ app/src/main/java/com/example/firestar/auth/LoginActivity.kt
@@ -3,19 +3,27 @@ package com.example.firestar.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.firestar.MainActivity
 import com.example.firestar.R
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.google.firebase.auth.FirebaseAuth
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var firebaseAuth: FirebaseAuth
+    val objectMapper = jacksonObjectMapper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +41,30 @@ class LoginActivity : AppCompatActivity() {
                 finish()
             }
         }
+
+        val client = OkHttpClient()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val mediaType = "application/x-www-form-urlencoded".toMediaType()
+            val body = "linkFile1=https://admin.thecricketer.com/weblab/Sites/96c8b790-b593-bfda-0ba4-ecd3a9fdefc2/resources/images/site/kohliheadshot-min.jpg&linkFile2=https://i.pinimg.com/originals/f0/5e/58/f05e58ea32f314a7bb39e2306ef90c14.png".toRequestBody(mediaType)
+            val request = Request.Builder()
+                .url("https://face-verification2.p.rapidapi.com/faceverification")
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("X-RapidAPI-Host", "face-verification2.p.rapidapi.com")
+                .addHeader("X-RapidAPI-Key", "9d366f6cbdmsh32b7c6b43e6bcdep14c956jsn81c1a47b0532")
+                .build()
+
+            client.newCall(request).execute().use {
+                val parsedResponse = parseResponse(it)
+                println(parsedResponse.toString())
+                println(parsedResponse["data"]["resultMessage"])
+                val result = parsedResponse["data"]["similarPercent"].toString().toDouble()
+                if(result > 0.80) println("YES")
+                else println("NO")
+            }
+        }
+
     }
 
     private fun loginUser(){
@@ -60,9 +92,15 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    fun parseResponse(response : Response): JsonNode {
+        val body = response.body?.string() ?: ""
+        val jsonBody = objectMapper.readValue<JsonNode>(body)
+        return jsonBody
+    }
+
     override fun onStart() {
         super.onStart()
-        if(firebaseAuth.currentUser != null){
+        if (firebaseAuth.currentUser != null) {
             Intent(this@LoginActivity, MainActivity::class.java).also {
                 startActivity(it)
                 finish()
